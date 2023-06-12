a1f4c33a9630c183df08679dec381edc002cab49
Jenna-r-ortega <135469375+Jenna-r-ortega@users.noreply.github.com>
2023-06-12 18:17:31 +0100

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,14 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning Jenna Repository"
+  git clone https://github.com/GreyMatterbots/url-auto-delete-shortener-bot /Jenna 
+else
+  echo "Cloning Jenna Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Jenna 
+fi
+cd /Jenna 
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 Jenna.py
+echo "jenna ortega"
+echo "started"
